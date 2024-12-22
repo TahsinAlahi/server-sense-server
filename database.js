38b99d361928640b4789ea8c1c7c65d2049da8da
Tahsin <74871776+TahsinAlahi@users.noreply.github.com>
2024-12-22 10:03:30 +0000

    created: database.js

--- database.js
+++ database.js
@@ -1,1 +1,9 @@
+const { MongoClient } = require("mongodb");
+const mongoUri = process.env.MONGO_URI;
 
+const client = new MongoClient(mongoUri);
+const database = client.db("serve-sense");
+const servicesCollection = database.collection("services");
+const reviewCollection = database.collection("reviews");
+
+module.exports = { client, servicesCollection, reviewCollection };
