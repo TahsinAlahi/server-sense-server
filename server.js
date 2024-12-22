c9858824b512874064534f868bdfe26d08ce488e
Tahsin <74871776+TahsinAlahi@users.noreply.github.com>
2024-12-22 20:31:04 +0000

    updated: setted up database

---
+++ server.js
@@ -0,0 +1,13 @@
+const app = require("./app");
+const { client } = require("./database");
+const port = process.env.PORT || 5000;
+
+client
+  .connect()
+  .then(() => {
+    console.log("Connected to MongoDB successfully");
+    app.listen(port, () => {
+      console.log(`Server running on port http://localhost:${port}`);
+    });
+  })
+  .catch((error) => console.error("Error connecting to MongoDB:", error));
