02fafab7e1d1948605dba38e63dd5726f38919b8
Tahsin <74871776+TahsinAlahi@users.noreply.github.com>
2024-12-22 20:42:54 +0000

    updated: added error handling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
+const createHttpErrors = require("http-errors");
 
 const app = express();
 
@@ -9,6 +10,22 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.use("*", (req, res, next) => {
+  next(createHttpErrors(404, "Route not found"));
+});
+
+app.use((err, req, res, next) => {
+  let errorCode = 500;
+  let errorMessage = "Something went wrong";
+
+  if (createHttpErrors.isHttpError(err)) {
+    errorCode = err.status;
+    errorMessage = err.message;
+  }
+
+  res.status(errorCode).json({ message: errorMessage });
 });
 
 module.exports = app;
