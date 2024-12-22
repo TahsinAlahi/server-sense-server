6bdf229c5d4cdba029c3e5d8ab3b32e5d47d794d
Tahsin <74871776+TahsinAlahi@users.noreply.github.com>
2024-12-22 21:23:51 +0000

    updated: added jwt token posting

--- app.js
+++ app.js
@@ -2,14 +2,33 @@ require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const createHttpErrors = require("http-errors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/api/jwt", async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    if (!email) throw createHttpErrors(400, "Email is required");
+    const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+      expiresIn: "3h",
+    });
+    res
+      .cookie("token", token, { httpOnly: true, secure: false })
+      .status(200)
+      .json({ message: "Token issued successfully" });
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.use("/api/services", require("./routes/services.route"));
