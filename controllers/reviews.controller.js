ceffb51ab9e2b1e1fafeba657f5b83981a909e4c
Tahsin <74871776+TahsinAlahi@users.noreply.github.com>
2024-12-23 21:02:20 +0000

    updated: review

--- controllers/reviews.controller.js
+++ controllers/reviews.controller.js
@@ -43,4 +43,24 @@ async function getMyReviews(req, res, next) {
   }
 }
 
-module.exports = { addNewReview, getMyReviews };
+async function updateReview(req, res, next) {
+  const { id } = req.params;
+  try {
+    if (!id) throw createHttpErrors(400, "Id is required");
+    if (!ObjectId.isValid(id)) throw createHttpErrors(400, "Invalid id");
+    const { review, rating } = req.body;
+    const result = await reviewCollection.findOneAndUpdate(
+      { _id: ObjectId.createFromHexString(id) },
+      { $set: { review, rating } },
+      { returnDocument: "after" }
+    );
+
+    if (!result) throw createHttpErrors(404, "Review not found");
+
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { addNewReview, getMyReviews, updateReview };
--- routes/reviews.route.js
+++ routes/reviews.route.js
@@ -5,5 +5,6 @@ const jwtVerify = require("../middlewares/jwt.middleware");
 
 router.post("/new-review", jwtVerify, reviewsController.addNewReview);
 router.get("/my-reviews", jwtVerify, reviewsController.getMyReviews);
+router.route("/review/:id").patch(jwtVerify, reviewsController.updateReview);
 
 module.exports = router;
